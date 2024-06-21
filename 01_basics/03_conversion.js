bc3bf6a9bcadb7f4894f7f426e5cce7cc4456526
Neeraj Chouhan <160119497+NeerajChouhan337@users.noreply.github.com>
2024-06-21 10:35:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,12 @@
+let score = "Neeraj";
+// console.log(typeof(score));
+// console.log(typeof(score));
+
+let val = Number(score);
+console.log(val);
+// console.log(typeof val);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 false => 0
