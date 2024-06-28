ddd57e3828bc6c5aed076424cdf3960dce0a7292
Neeraj Chouhan <160119497+NeerajChouhan337@users.noreply.github.com>
2024-06-28 11:32:05 +0000

    object 2

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -16,10 +16,19 @@
 // }
 // // console.log(user.fullname.userFullname.firstName);
 
-const obj1 = {1:"a",2:"b",3:"c"};
-const obj2 = {4:"d",5:"e",6:"f"}
-// const obj3 = {obj1,obj2};
+// const obj1 = {1:"a",2:"b",3:"c"};
+// const obj2 = {4:"d",5:"e",6:"f"}
+// // const obj3 = {obj1,obj2};
+// // console.log(obj3);
+
+// const obj3 = Object.assign({},obj1,obj2)
 // console.log(obj3);
 
-const obj3 = Object.assign({},obj1,obj2)
-console.log(obj3);
+
+const course ={
+    name:"Neeraj",
+    courseFee: 999,
+    courseInstructor:"hitesh"
+}
+const{courseInstructor}=course;
+console.log(courseInstructor);
