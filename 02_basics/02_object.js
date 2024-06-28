c820c9e001d3f1ba588bd7f4654f80618d14fcee
Neeraj Chouhan <160119497+NeerajChouhan337@users.noreply.github.com>
2024-06-28 11:18:03 +0000

    object 2

---
+++ 02_basics/02_object.js
@@ -0,0 +1,25 @@
+// const tinderUser= new Object() singleton object
+
+// const tinderUser = {}
+
+// tinderUser.id = "123abc";
+// tinderUser.name = "sammy";
+
+// const user ={
+//     email:"neeraj@gmail.com",
+//     fullname:{
+//         userFullname:{
+//             firstName:"neeraj",
+//             lastName:"chouhan"
+//         }
+//     }
+// }
+// // console.log(user.fullname.userFullname.firstName);
+
+const obj1 = {1:"a",2:"b",3:"c"};
+const obj2 = {4:"d",5:"e",6:"f"}
+// const obj3 = {obj1,obj2};
+// console.log(obj3);
+
+const obj3 = Object.assign({},obj1,obj2)
+console.log(obj3);
